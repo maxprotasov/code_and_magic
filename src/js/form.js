c20e976f60704ac382fba0c40d35013eb781481d
proektiruet <proektiruet@gmail.com>
2016-10-15 16:53:46 +0300

    Добавлена валидация формы отзыва

--- src/js/form.js
+++ src/js/form.js
@@ -32,3 +32,62 @@ window.form = (function() {
 
   return form;
 })();
+
+var formElements = document.querySelector('.review-form').elements;
+
+var marksBlock = document.querySelector('.review-form-group-mark');
+var marks = formElements['review-mark'];
+
+var nameField = formElements['review-name'];
+var textField = formElements['review-text'];
+
+var nameLink = document.querySelector('.review-fields-name');
+var textLink = document.querySelector('.review-fields-text');
+var submitBtn = document.querySelector('.review-submit');
+var linksBlock = document.querySelector('.review-fields');
+
+nameField.required = true;
+textLink.classList.add('invisible');
+submitBtn.disabled = true;
+
+function toggleClass(element, className, shouldAdd) {
+
+  if (shouldAdd) {
+    element.classList.add(className);
+  } else {
+    element.classList.remove(className);
+  }
+
+}
+
+function getRadioValue() {
+  for (var i = 0; i < marks.length; i++) {
+
+    if (marks[i].checked) {
+      return marks[i].value;
+    }
+
+  }
+  return '';
+}
+
+function checkFormValid() {
+  var isFormValid = nameField.validity.valid && textField.validity.valid;
+
+  submitBtn.disabled = !isFormValid;
+  toggleClass(linksBlock, 'invisible', isFormValid);
+  toggleClass(textLink, 'invisible', textField.validity.valid);
+  toggleClass(nameLink, 'invisible', nameField.validity.valid);
+}
+
+marksBlock.onchange = function() {
+  var isRatingBad = Number(getRadioValue()) <= 2;
+
+  textField.required = isRatingBad;
+
+  checkFormValid();
+};
+
+nameField.oninput = checkFormValid;
+
+textField.oninput = checkFormValid;
